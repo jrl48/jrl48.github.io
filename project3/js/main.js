b7ae11dff93a218ee04a30fd4046eda431841efe
Joseph Lilien <jrl48@duke.edu>
2017-10-31 20:38:18 -0400

    finished prob 1

--- project3/js/main.js
+++ project3/js/main.js
@@ -1,10 +1,10 @@
 // p1
-function p1(x) {
-    return 5*x – 3*x^2 + 13;
+function p1f(x) {
+    return 5 * x - 3 * (x * x) + 13;
 }
+
 function p1e() {
-    var x = document.getElementById("p1in");
-    var p1 = document.getElementById("p1");
-    p1.textContent = "kldj";
+    var x = document.getElementById("p1in").value;
+    document.getElementById("p1").textContent = p1f(x);
 }
 
