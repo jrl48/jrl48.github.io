1633216e470695b3487bebbc3afaa3fbb84df35b
Joseph Lilien <jrl48@duke.edu>
2017-10-31 20:26:57 -0400

    work on prob 1

---
+++ project3/js/main.js
@@ -0,0 +1,10 @@
+// p1
+function p1(x) {
+    return 5*x – 3*x^2 + 13;
+}
+function p1e() {
+    var x = document.getElementById("p1in");
+    var p1 = document.getElementById("p1");
+    p1.textContent = "kldj";
+}
+
