5a5d52ba6f5a1efc32e574302eb5faddb49b8692
Joseph Lilien <jrl48@duke.edu>
2017-11-01 12:42:53 -0400

    prob 2 done

--- project3/js/main.js
+++ project3/js/main.js
@@ -8,3 +8,15 @@ function p1e() {
     document.getElementById("p1").textContent = p1f(x);
 }
 
+// p2
+function p2f(x, y, z) {
+    var output = "Hello, " + x + " " + y + " " + z + "!";
+    return output;
+}
+
+function p2e() {
+    var x = document.getElementById("p2x").value,
+        y = document.getElementById("p2y").value,
+        z = document.getElementById("p2z").value;
+    document.getElementById("p2").textContent = p2f(x, y, z);
+}
